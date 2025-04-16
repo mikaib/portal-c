ba8967f5328c2e8bc77ef522b35fb405e808c64a
http80 <70071615+http-80@users.noreply.github.com>
2025-04-16 21:22:34 +0200

    init

---
+++ portal.c
@@ -0,0 +1,5 @@
+//
+// Created by mikai on 4/16/2025.
+//
+
+#include "portal.h"
---
+++ portal.h
@@ -0,0 +1,8 @@
+//
+// Created by mikai on 4/16/2025.
+//
+
+#ifndef PORTAL_H
+#define PORTAL_H
+
+#endif //PORTAL_H
